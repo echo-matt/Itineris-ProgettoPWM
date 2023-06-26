d4d6637ed1ae2bee781c835fa574d976695251d1
echo-matt <matteopaterno98@gmail.com>
2023-06-26 17:47:16 +0200

    Retrofit implementation e UserAPI

--- app/src/main/java/com/matteopaterno/progettopwm/MainActivity.kt
+++ app/src/main/java/com/matteopaterno/progettopwm/MainActivity.kt
@@ -2,11 +2,24 @@ package com.matteopaterno.progettopwm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.FragmentManager
 import com.matteopaterno.progettopwm.databinding.ActivityMainBinding
 import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
+
+    val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8000/webmobile/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(UserAPI::class.java)
+    }
+
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,9 +35,10 @@ class MainActivity : AppCompatActivity() {
                 transaction.setReorderingAllowed(true)
                 transaction.replace(R.id.fragmentLoginRegistrationView, loginFragment)
                 transaction.addToBackStack("Login Fragment")
+                transaction.commit()
             }
             transaction.show(loginFragment)
-            transaction.commit()
+
         }
         binding.registerButton.setOnClickListener {
             val manager = supportFragmentManager
@@ -32,20 +46,27 @@ class MainActivity : AppCompatActivity() {
             if (!registerFragment.isAdded){
                 transaction.setReorderingAllowed(true)
                 transaction.replace(R.id.fragmentLoginRegistrationView, registerFragment)
-                transaction.addToBackStack("Login Fragment")
+                transaction.addToBackStack("Register Fragment")
+                transaction.commit()
             }
             transaction.show(registerFragment)
-            transaction.commit()
         }
         setContentView(binding.root)
     }
 
     override fun onBackPressed() {
-        if (supportFragmentManager.backStackEntryCount > 0){
-            supportFragmentManager.popBackStack(null, POP_BACK_STACK_INCLUSIVE)
-        }else {
-            super.onBackPressed()
+        val fragmentManger = supportFragmentManager
+        val entryInBackStack = fragmentManger.backStackEntryCount
+        for (entry in 0 until entryInBackStack){
+            fragmentManger.popBackStack()
         }
+        printBackStack("a", entryInBackStack)
+    }
+
+    private fun printBackStack(text: String, entryInBackStack: Int){
+        var showMyFragments: String = text
+        showMyFragments += "Number of Fragments: $entryInBackStack"
+        Toast.makeText(this, showMyFragments, Toast.LENGTH_SHORT).show()
     }
 
 }
---
+++ app/src/main/java/com/matteopaterno/progettopwm/UserAPI.kt
@@ -0,0 +1,27 @@
+package com.matteopaterno.progettopwm;
+
+import com.google.gson.JsonObject
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET
+import retrofit2.http.POST;
+import retrofit2.http.Url
+
+public interface UserAPI {
+    @POST("postSelect/")
+    @FormUrlEncoded
+    fun select(@Field("query") query: String): Call <JsonObject>
+    @POST("postUpdate/")
+    @FormUrlEncoded
+    fun update(@Field("query") query: String): Call <JsonObject>
+    @POST("postRemove/")
+    @FormUrlEncoded
+    fun remove(@Field("query") query: String): Call<JsonObject>
+    @POST("postInsert/")
+    @FormUrlEncoded
+    fun unsert(@Field("query") query: String): Call <JsonObject>
+    @GET
+    fun image(@Url url: String) : Call <ResponseBody>
+}
